2f7fb8ae7aea5cc46522602721c8bdc9efa73a96
mrsmrynk <marius.maryniak@w-hs.de>
2022-07-23 20:38:59 +0000

    test: add tests for coordinates

---
+++ src/preprocessing/tests/__init__.py
@@ -0,0 +1,3 @@
+# @author: Maryniak, Marius - Fachbereich Elektrotechnik, Westfälische Hochschule Gelsenkirchen
+
+from src.preprocessing.tests.test_coordinates import *
---
+++ src/preprocessing/tests/test_coordinates.py
@@ -0,0 +1,49 @@
+# @author: Maryniak, Marius - Fachbereich Elektrotechnik, Westfälische Hochschule Gelsenkirchen
+
+import pytest
+
+from src.preprocessing.coordinates import get_coordinates
+
+
+@pytest.mark.parametrize('test_input, expected',
+                         [((256, 256, 512, 512), [(256., 384.), (384., 384.), (256., 512.), (384., 512.)]),
+                          ((256, -512, 512, -256), [(256., -384.), (384., -384.), (256., -256.), (384., -256.)]),
+                          ((-512, -512, -256, -256), [(-512., -384.), (-384., -384.), (-512., -256.), (-384., -256.)]),
+                          ((-512, 256, -256, 512), [(-512., 384.), (-384., 384.), (-512., 512.), (-384., 512.)]),
+                          ((-128, -128, 128, 128), [(-128., 0.), (0., 0.), (-128., 128.), (0., 128.)])])
+def test_get_coordinates(test_input, expected):
+    """Tests get_coordinates() with different bounding boxes and an image_size of 640.
+    The tiles fit in the bounding boxes without remainder.
+
+    :param (int, int, int, int) test_input: bounding box (x_1, y_1, x_2, y_2) of the area from
+        the bottom left corner to the top right corner
+    :param list[(float, float)] expected: coordinates
+    :returns: None
+    :rtype: None
+    """
+    test_coordinates = get_coordinates(bounding_box=test_input,
+                                       image_size=640)
+    coordinates = expected
+    assert test_coordinates == coordinates
+
+
+@pytest.mark.parametrize('test_input, expected',
+                         [((256, 256, 480, 480), [(256., 384.), (384., 384.), (256., 512.), (384., 512.)]),
+                          ((256, -512, 480, -288), [(256., -384.), (384., -384.), (256., -256.), (384., -256.)]),
+                          ((-512, -512, -288, -288), [(-512., -384.), (-384., -384.), (-512., -256.), (-384., -256.)]),
+                          ((-512, 256, -288, 480), [(-512., 384.), (-384., 384.), (-512., 512.), (-384., 512.)]),
+                          ((-128, -128, 96, 96), [(-128., 0.), (0., 0.), (-128., 128.), (0., 128.)])])
+def test_get_coordinates_remainder(test_input, expected):
+    """Tests get_coordinates() with different bounding boxes and an image_size of 640.
+    The tiles do not fit in the bounding boxes without remainder.
+
+    :param (int, int, int, int) test_input: bounding box (x_1, y_1, x_2, y_2) of the area from
+        the bottom left corner to the top right corner
+    :param list[(float, float)] expected: coordinates
+    :returns: None
+    :rtype: None
+    """
+    test_coordinates = get_coordinates(bounding_box=test_input,
+                                       image_size=640)
+    coordinates = expected
+    assert test_coordinates == coordinates
