6367bbac49ce51e956a223c30a5c02df1b2064e8
mrsmrynk <marius.maryniak@w-hs.de>
2022-07-24 11:31:50 +0000

    style: reformat docstring

--- src/preprocessing/coordinates.py
+++ src/preprocessing/coordinates.py
@@ -6,10 +6,9 @@ import src.utils as utils
 def get_coordinates(bounding_box, image_size):
     """Returns the coordinates of the top left corner of each tile in the area of the bounding_box.
 
-    :param (int, int, int, int) bounding_box: bounding box (x_1, y_1, x_2, y_2) of the area from
-        the bottom left corner to the top right corner
+    :param (int, int, int, int) bounding_box: bounding box (x_1, y_1, x_2, y_2)
     :param int image_size: image size in pixels
-    :returns: coordinates
+    :returns: coordinates (x, y) of each tile
     :rtype: list[(float, float)]
     """
     image_size_meters = image_size * utils.RESOLUTION
