bbc1c30fdd4680d72e8dd084afb028ee52335f4c
mrsmrynk <marius.maryniak@w-hs.de>
2022-07-28 15:05:25 +0000

    feat: add export_features() in postprocessor

--- src/postprocessing/postprocessor.py
+++ src/postprocessing/postprocessor.py
@@ -1,5 +1,8 @@
 # @author: Maryniak, Marius - Fachbereich Elektrotechnik, Westfälische Hochschule Gelsenkirchen
 
+import json
+from pathlib import Path
+
 import geopandas as gpd
 import numpy as np
 import rasterio as rio
@@ -11,40 +14,57 @@ import src.utils as utils
 
 class Postprocessor:
     def __init__(self,
-                 resolution,
-                 sieve_size):
+                 sieve_size,
+                 output_dir_path):
         """Constructor method
 
-        :param float resolution: resolution in meters per pixel
         :param int or None sieve_size: sieve size in pixels (minimum number of pixels to retain)
+        :param str or Path output_dir_path: path to the output directory
         :returns: None
         :rtype: None
         """
-        self.resolution = resolution
         self.sieve_size = sieve_size
+        self.features_dir_path = Path(output_dir_path) / '.features'
+
+    def export_features(self,
+                        features,
+                        coordinates):
+        """Exports features as a features file (.json) to the .features directory.
+        Each features file name is in the following schema: x_y.json
+
+        :param list[dict[str, dict[str, Any]]] features: features
+        :param (int, int) coordinates: coordinates (x, y)
+        :returns: None
+        :rtype: None
+        """
+        path = self.features_dir_path / f'{coordinates[0]}_{coordinates[1]}.json'
+        with open(path, mode='w') as file:
+            json.dump(features, file, indent=4)
 
     def vectorize_mask(self,
                        mask,
                        coordinates):
-        """Returns georeferenced features of the polygons in the mask given its coordinates of the top left corner.
+        """Exports georeferenced features of the polygons in the vectorized mask given its coordinates
+        of the top left corner as a features file (.json) to the .features directory.
 
         :param np.ndarray[np.uint8] mask: mask
-        :param (float, float) coordinates: coordinates (x, y)
-        :returns: features
-        :rtype: dict
+        :param (int, int) coordinates: coordinates (x, y)
+        :returns: None
+        :rtype: None
         """
         if self.sieve_size is not None:
             mask = rio.features.sieve(mask, size=self.sieve_size)
 
         transform = rio.transform.from_origin(west=coordinates[0],
                                               north=coordinates[1],
-                                              xsize=self.resolution,
-                                              ysize=self.resolution)
+                                              xsize=utils.RESOLUTION,
+                                              ysize=utils.RESOLUTION)
         vectorized_mask = rio.features.shapes(mask, transform=transform)
 
         features = [{'properties': {'class': int(value)}, 'geometry': shape}
                     for shape, value in vectorized_mask if int(value) != 0]
-        return features
+        self.export_features(features=features,
+                             coordinates=coordinates)
 
     @staticmethod
     def get_gdf(features):
