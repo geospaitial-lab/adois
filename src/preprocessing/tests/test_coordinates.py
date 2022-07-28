4ba598d16451a1d05b7faa5414cdf185dd739bf4
mrsmrynk <marius.maryniak@w-hs.de>
2022-07-28 11:09:05 +0000

    refactor: refactor test_coordinates

--- src/preprocessing/tests/test_coordinates.py
+++ src/preprocessing/tests/test_coordinates.py
@@ -6,11 +6,16 @@ from src.preprocessing.coordinates import get_coordinates
 
 
 @pytest.mark.parametrize('test_input, expected',
-                         [((256, 256, 512, 512), [(256., 384.), (384., 384.), (256., 512.), (384., 512.)]),
-                          ((256, -512, 512, -256), [(256., -384.), (384., -384.), (256., -256.), (384., -256.)]),
-                          ((-512, -512, -256, -256), [(-512., -384.), (-384., -384.), (-512., -256.), (-384., -256.)]),
-                          ((-512, 256, -256, 512), [(-512., 384.), (-384., 384.), (-512., 512.), (-384., 512.)]),
-                          ((-128, -128, 128, 128), [(-128., 0.), (0., 0.), (-128., 128.), (0., 128.)])])
+                         [((512, 512, 1024, 1024),
+                           [(512., 768.), (768., 768.), (512., 1024.), (768., 1024.)]),
+                          ((512, -1024, 1024, -512),
+                           [(512., -768.), (768., -768.), (512., -512.), (768., -512.)]),
+                          ((-1024, -1024, -512, -512),
+                           [(-1024., -768.), (-768., -768.), (-1024., -512.), (-768., -512.)]),
+                          ((-1024, 512, -512, 1024),
+                           [(-1024., 768.), (-768., 768.), (-1024., 1024.), (-768., 1024.)]),
+                          ((-256, -256, 256, 256),
+                           [(-256., 0.), (0., 0.), (-256., 256.), (0., 256.)])])
 def test_get_coordinates(test_input, expected):
     """Tests get_coordinates() with different bounding boxes and an image_size of 640.
     The tiles fit in the bounding boxes without remainder.
@@ -22,7 +27,7 @@ def test_get_coordinates(test_input, expected):
     :rtype: None
     """
     coordinates = get_coordinates(bounding_box=test_input,
-                                  image_size=640)
+                                  image_size=1280)
     assert coordinates == expected
 
     for coordinates_element in coordinates:
@@ -31,11 +36,16 @@ def test_get_coordinates(test_input, expected):
 
 
 @pytest.mark.parametrize('test_input, expected',
-                         [((256, 256, 480, 480), [(256., 384.), (384., 384.), (256., 512.), (384., 512.)]),
-                          ((256, -512, 480, -288), [(256., -384.), (384., -384.), (256., -256.), (384., -256.)]),
-                          ((-512, -512, -288, -288), [(-512., -384.), (-384., -384.), (-512., -256.), (-384., -256.)]),
-                          ((-512, 256, -288, 480), [(-512., 384.), (-384., 384.), (-512., 512.), (-384., 512.)]),
-                          ((-128, -128, 96, 96), [(-128., 0.), (0., 0.), (-128., 128.), (0., 128.)])])
+                         [((512, 512, 896, 896),
+                           [(512., 768.), (768., 768.), (512., 1024.), (768., 1024.)]),
+                          ((512, -1024, 896, -640),
+                           [(512., -768.), (768., -768.), (512., -512.), (768., -512.)]),
+                          ((-1024, -1024, -640, -640),
+                           [(-1024., -768.), (-768., -768.), (-1024., -512.), (-768., -512.)]),
+                          ((-1024, 512, -640, 896),
+                           [(-1024., 768.), (-768., 768.), (-1024., 1024.), (-768., 1024.)]),
+                          ((-256, -256, 128, 128),
+                           [(-256., 0.), (0., 0.), (-256., 256.), (0., 256.)])])
 def test_get_coordinates_remainder(test_input, expected):
     """Tests get_coordinates() with different bounding boxes and an image_size of 640.
     The tiles do not fit in the bounding boxes without remainder.
@@ -47,7 +57,7 @@ def test_get_coordinates_remainder(test_input, expected):
     :rtype: None
     """
     coordinates = get_coordinates(bounding_box=test_input,
-                                  image_size=640)
+                                  image_size=1280)
     assert coordinates == expected
 
     for coordinates_element in coordinates:
