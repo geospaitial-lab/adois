d45a3ba3df535be260973412ab150e82ffdbc0af
mrsmrynk <mrsmrynk@gmail.com>
2022-08-11 15:35:37 +0200

    refactor: refactor argument_parser

--- src/utils/argument_parser.py
+++ src/utils/argument_parser.py
@@ -1,5 +1,6 @@
 # @author: Maryniak, Marius - Fachbereich Elektrotechnik, Westfälische Hochschule Gelsenkirchen
 
+import argparse
 from argparse import ArgumentParser
 from typing import List, Optional, Union
 
@@ -21,57 +22,74 @@ def get_argument_parser():
 
     argument_parser.add_argument('--wms_url_rgb',
                                  type=str,
+                                 default=argparse.SUPPRESS,
                                  help='url of the web map service (rgb)')
     argument_parser.add_argument('--wms_layer_rgb',
                                  type=str,
+                                 default=argparse.SUPPRESS,
                                  help='layer of the web map service (rgb)')
     argument_parser.add_argument('--wms_url_nir',
                                  type=str,
+                                 default=argparse.SUPPRESS,
                                  help='url of the web map service (nir)')
     argument_parser.add_argument('--wms_layer_nir',
                                  type=str,
+                                 default=argparse.SUPPRESS,
                                  help='layer of the web map service (nir)')
     argument_parser.add_argument('--wms_url_ndsm',
                                  type=str,
+                                 default=argparse.SUPPRESS,
                                  help='url of the web map service (ndsm)')
     argument_parser.add_argument('--wms_layer_ndsm',
                                  type=str,
+                                 default=argparse.SUPPRESS,
                                  help='layer of the web map service (ndsm)')
     argument_parser.add_argument('--epsg_code',
                                  type=int,
+                                 default=argparse.SUPPRESS,
                                  help='epsg code of the coordinate reference system')
     argument_parser.add_argument('--boundary_shape_file_path',
                                  type=Optional[str],
+                                 default=argparse.SUPPRESS,
                                  help='path to the boundary shape file (.shp)')
     argument_parser.add_argument('--bounding_box',
                                  type=Optional[List[int]],
+                                 default=argparse.SUPPRESS,
                                  help='bounding box (x_1, y_1, x_2, y_2)')
 
     argument_parser.add_argument('--color_codes_ndsm',
                                  type=List[str],
+                                 default=argparse.SUPPRESS,
                                  help='color codes for the color mapping')
 
     argument_parser.add_argument('--sieve_size',
                                  type=Optional[int],
+                                 default=argparse.SUPPRESS,
                                  help='sieve size in square meters')
     argument_parser.add_argument('--simplify',
                                  type=Optional[bool],
+                                 default=argparse.SUPPRESS,
                                  help='if True, the shape file is simplified using the Douglas-Peucker algorithm')
 
     argument_parser.add_argument('--tile_size',
                                  type=Optional[Union[int, List[Union[int, None]]]],
+                                 default=argparse.SUPPRESS,
                                  help='tile size in meters')
     argument_parser.add_argument('--shape_file_path',
                                  type=Optional[Union[str, List[Union[str, None]]]],
+                                 default=argparse.SUPPRESS,
                                  help='path to the boundary shape file (.shp)')
 
     argument_parser.add_argument('--output_dir_path',
                                  type=str,
+                                 default=argparse.SUPPRESS,
                                  help='path to the output directory')
     argument_parser.add_argument('--prefix',
                                  type=str,
+                                 default=argparse.SUPPRESS,
                                  help='prefix of the shape file names')
     argument_parser.add_argument('--export_raw_shape_file',
                                  type=Optional[bool],
+                                 default=argparse.SUPPRESS,
                                  help='if True, a shape file without postprocessing is exported')
     return argument_parser
