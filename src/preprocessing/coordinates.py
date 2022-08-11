47eb40d6a759279c87fd0bbdcd7e3576f7f3b6e0
mrsmrynk <mrsmrynk@gmail.com>
2022-08-11 19:00:33 +0200

    feat: add get_internal_coordinates() in coordinates

--- src/preprocessing/coordinates.py
+++ src/preprocessing/coordinates.py
@@ -3,7 +3,10 @@
 import re
 from pathlib import Path
 
+import geopandas as gpd
+
 import src.utils as utils
+from src.aggregation.grid_generator import GridGenerator
 
 
 def get_coordinates(bounding_box):
@@ -37,8 +40,32 @@ def get_coordinates(bounding_box):
     return coordinates
 
 
-def filter_coordinates(coordinates, output_dir_path):
-    """Returns the filtered coordinates. If a tile is already being processed (its shape file directory in
+def get_internal_coordinates(bounding_box,
+                             epsg_code,
+                             boundary_gdf):
+    """Returns the coordinates of the top left corner of each tile in the area of the boundary geodataframe.
+
+    :param (int, int, int, int) bounding_box: bounding box (x_1, y_1, x_2, y_2)
+    :param int epsg_code: epsg code of the coordinate reference system
+    :param gpd.GeoDataFrame boundary_gdf: boundary geodataframe
+    :returns: internal coordinates (x, y) of each tile
+    :rtype: list[(int, int)]
+    """
+    coordinates = get_coordinates(bounding_box)
+
+    grid_generator = GridGenerator(bounding_box=bounding_box,
+                                   epsg_code=epsg_code)
+    grid_gdf = grid_generator.get_grid(tile_size_meters=utils.IMAGE_SIZE_METERS)
+
+    valid_coordinates = list(grid_gdf['geometry'].intersects(boundary_gdf['geometry'][0]))
+    coordinates = [coordinates_element for (coordinates_element, valid) in zip(coordinates, valid_coordinates)
+                   if valid]
+
+    return coordinates
+
+
+def filter_downloaded_coordinates(coordinates, output_dir_path):
+    """Returns the filtered coordinates. If a tile is already being downloaded (its shape file directory in
     .tiles directory exists), its coordinates are removed.
 
     :param list[(int, int)] coordinates: coordinates (x, y) of each tile
