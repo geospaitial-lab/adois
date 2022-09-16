da3fb92ef86ae461b12358cdd43653009b9ff435
mrsmrynk <mrsmrynk@gmail.com>
2022-09-16 10:37:23 +0200

    style: reformat docstrings

--- src/inference/inference.py
+++ src/inference/inference.py
@@ -6,7 +6,8 @@ import onnxruntime
 
 class Inference:
     def __init__(self, model_path):
-        """Constructor method
+        """
+        | Constructor method
 
         :param str model_path: path to the model (.onnx)
         :returns: None
@@ -16,10 +17,11 @@ class Inference:
         self.model_input_name = self.model.get_inputs()[0].name
 
     def get_mask(self, image):
-        """Returns a mask with 2 dimensions and the following pixel values:
-        0 - pervious surfaces
-        1 - buildings
-        2 - impervious surfaces
+        """
+        | Returns a mask with 2 dimensions and the following pixel values:
+        | - 0: pervious surfaces
+        | - 1: buildings
+        | - 2: impervious surfaces
 
         :param np.ndarray[np.float32] image: image
         :returns: mask
