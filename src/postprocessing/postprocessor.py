4e3636a762e0597f4b2f95d5b13dd96de1a1c25e
mrsmrynk <mrsmrynk@gmail.com>
2022-09-16 10:40:26 +0200

    style: reformat docstrings

--- src/postprocessing/postprocessor.py
+++ src/postprocessing/postprocessor.py
@@ -22,7 +22,8 @@ class Postprocessor:
                  bounding_box,
                  epsg_code,
                  boundary_gdf):
-        """Constructor method
+        """
+        | Constructor method
 
         :param str or Path output_dir_path: path to the output directory
         :param (int, int, int, int) bounding_box: bounding box (x_1, y_1, x_2, y_2)
@@ -39,8 +40,9 @@ class Postprocessor:
     def export_features(self,
                         features,
                         coordinates):
-        """Exports features of a tile as a shape file (.shp) in a subdirectory to the .tiles directory.
-        Each subdirectory name is in the following schema: x_y
+        """
+        | Exports features of a tile as a shape file (.shp) in a subdirectory to the .tiles directory.
+            Each subdirectory name is in the following schema: x_y
 
         :param list[dict[str, dict[str, Any]]] features: features
         :param (int, int) coordinates: coordinates (x, y)
@@ -61,8 +63,9 @@ class Postprocessor:
     def vectorize_mask(self,
                        mask,
                        coordinates):
-        """Exports a shape file (.shp) of the polygons in the vectorized mask given its coordinates
-        of the top left corner in a subdirectory to the .tiles directory.
+        """
+        | Exports a shape file (.shp) of the polygons in the vectorized mask given its coordinates
+            of the top left corner in a subdirectory to the .tiles directory.
 
         :param np.ndarray[np.uint8] mask: mask
         :param (int, int) coordinates: coordinates (x, y)
@@ -81,7 +84,8 @@ class Postprocessor:
                              coordinates=coordinates)
 
     def concatenate_gdfs(self, coordinates):
-        """Returns a concatenated geodataframe.
+        """
+        | Returns a concatenated geodataframe.
 
         :param list[(int, int)] coordinates: coordinates (x, y) of each tile
         :returns: concatenated geodataframe
@@ -107,7 +111,8 @@ class Postprocessor:
 
     @staticmethod
     def sieve_gdf(gdf, sieve_size):
-        """Returns a sieved geodataframe.
+        """
+        | Returns a sieved geodataframe.
 
         :param gpd.GeoDataFrame gdf: geodataframe
         :param int sieve_size: sieve size in square meters (minimum area of polygons to retain)
@@ -120,8 +125,11 @@ class Postprocessor:
 
     @staticmethod
     def fill_polygon(polygon, hole_size):
-        """Returns a polygon without holes.
-        Based on: https://gis.stackexchange.com/a/409398
+        """
+        | Returns a polygon without holes.
+        |
+        | Based on:
+        | https://gis.stackexchange.com/a/409398
 
         :param Polygon polygon: polygon
         :param int hole_size: hole size in square meters (maximum area of holes in the polygons to retain)
@@ -140,8 +148,12 @@ class Postprocessor:
 
     @staticmethod
     def fill_gdf(gdf, hole_size):
-        """Returns a geodataframe without holes in the polygons.
-        Based on: https://gis.stackexchange.com/a/409398 and https://stackoverflow.com/a/61466689
+        """
+        | Returns a geodataframe without holes in the polygons.
+        |
+        | Based on:
+        | https://gis.stackexchange.com/a/409398
+        | https://stackoverflow.com/a/61466689
 
         :param gpd.GeoDataFrame gdf: geodataframe
         :param int hole_size: hole size in square meters (maximum area of holes in the polygons to retain)
@@ -153,7 +165,8 @@ class Postprocessor:
         return gdf
 
     def simplify_gdf(self, gdf):
-        """Returns a geodataframe with simplified polygons (Douglas-Peucker algorithm is used).
+        """
+        | Returns a geodataframe with simplified polygons (Douglas-Peucker algorithm is used).
 
         :param gpd.GeoDataFrame gdf: geodataframe
         :returns: simplified geodataframe
@@ -164,7 +177,8 @@ class Postprocessor:
         return simplified_gdf
 
     def clip_gdf(self, gdf):
-        """Returns a clipped geodataframe.
+        """
+        | Returns a clipped geodataframe.
 
         :param gpd.GeoDataFrame gdf: geodataframe
         :returns: clipped geodataframe
