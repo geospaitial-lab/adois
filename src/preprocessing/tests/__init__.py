763e7b521f264ec05b2b7aee19f494b264bbf8c8
mrsmrynk <mrsmrynk@gmail.com>
2022-09-16 10:34:43 +0200

    refactor: refactor __init__

--- src/preprocessing/tests/__init__.py
+++ src/preprocessing/tests/__init__.py
@@ -1,5 +1,5 @@
 # @author: Maryniak, Marius - Fachbereich Elektrotechnik, Westfälische Hochschule Gelsenkirchen
 
-from src.preprocessing.tests.conftest import *
-from src.preprocessing.tests.test_coordinates import *
-from src.preprocessing.tests.test_preprocessor import *
+from .conftest import *
+from .test_coordinates import *
+from .test_preprocessor import *
