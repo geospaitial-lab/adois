de9c7ddd02c0282c6c6e5c5c5d5bfa8b42fd0a52
mrsmrynk <mrsmrynk@gmail.com>
2022-11-16 13:47:08 +0100

    edit comments

--- src/utils/tests/data/tests_data.py
+++ src/utils/tests/data/tests_data.py
@@ -1,7 +1,9 @@
 # @author: Maryniak, Marius - Fachbereich Elektrotechnik, Westfälische Hochschule Gelsenkirchen
 
 parameters_get_coordinates = \
-    [((512, 512, 1024, 1024),  # no quantization, no remainder
+    [
+     # region no quantization, no remainder
+     ((512, 512, 1024, 1024),
       [(512, 768), (768, 768), (512, 1024), (768, 1024)]),
      ((512, -1024, 1024, -512),
       [(512, -768), (768, -768), (512, -512), (768, -512)]),
@@ -11,7 +13,9 @@ parameters_get_coordinates = \
       [(-1024, 768), (-768, 768), (-1024, 1024), (-768, 1024)]),
      ((-256, -256, 256, 256),
       [(-256, 0), (0, 0), (-256, 256), (0, 256)]),
-     ((640, 640, 1024, 1024),  # quantization, no remainder
+     # endregion
+     # region quantization, no remainder
+     ((640, 640, 1024, 1024),
       [(512, 768), (768, 768), (512, 1024), (768, 1024)]),
      ((640, -896, 1024, -512),
       [(512, -768), (768, -768), (512, -512), (768, -512)]),
@@ -21,7 +25,9 @@ parameters_get_coordinates = \
       [(-1024, 768), (-768, 768), (-1024, 1024), (-768, 1024)]),
      ((-128, -128, 256, 256),
       [(-256, 0), (0, 0), (-256, 256), (0, 256)]),
-     ((512, 512, 896, 896),  # no quantization, remainder
+     # endregion
+     # region no quantization, remainder
+     ((512, 512, 896, 896),
       [(512, 768), (768, 768), (512, 1024), (768, 1024)]),
      ((512, -1024, 896, -640),
       [(512, -768), (768, -768), (512, -512), (768, -512)]),
@@ -31,7 +37,9 @@ parameters_get_coordinates = \
       [(-1024, 768), (-768, 768), (-1024, 1024), (-768, 1024)]),
      ((-256, -256, 128, 128),
       [(-256, 0), (0, 0), (-256, 256), (0, 256)]),
-     ((640, 640, 896, 896),  # quantization, remainder
+     # endregion
+     # region quantization, remainder
+     ((640, 640, 896, 896),
       [(512, 768), (768, 768), (512, 1024), (768, 1024)]),
      ((640, -896, 896, -640),
       [(512, -768), (768, -768), (512, -512), (768, -512)]),
@@ -40,7 +48,9 @@ parameters_get_coordinates = \
      ((-896, 640, -640, 896),
       [(-1024, 768), (-768, 768), (-1024, 1024), (-768, 1024)]),
      ((-128, -128, 128, 128),
-      [(-256, 0), (0, 0), (-256, 256), (0, 256)])]
+      [(-256, 0), (0, 0), (-256, 256), (0, 256)])
+    # endregion
+    ]
 
 parameters_filter_cached_coordinates_empty_tiles_dir = \
     [([(512, 768), (768, 768), (512, 1024), (768, 1024)],
@@ -55,7 +65,9 @@ parameters_filter_cached_coordinates_empty_tiles_dir = \
       [(-256, 0), (0, 0), (-256, 256), (0, 256)])]
 
 parameters_filter_cached_coordinates_not_empty_tiles_dir = \
-    [([(512, 768), (768, 768), (512, 1024), (768, 1024)],  # some tiles are already being processed
+    [
+     # region some tiles have already been downloaded
+     ([(512, 768), (768, 768), (512, 1024), (768, 1024)],
       [(512, 768), (768, 768)]),
      ([(512, -768), (768, -768), (512, -512), (768, -512)],
       [(512, -768), (768, -768)]),
@@ -65,7 +77,9 @@ parameters_filter_cached_coordinates_not_empty_tiles_dir = \
       [(-1024, 768), (-768, 768)]),
      ([(-256, 0), (0, 0), (-256, 256), (0, 256)],
       [(-256, 0), (0, 0)]),
-     ([(512, 1024), (768, 1024)],  # all tiles are already being processed
+     # endregion
+     # region all tiles have already been downloaded
+     ([(512, 1024), (768, 1024)],
       []),
      ([(512, -512), (768, -512)],
       []),
@@ -74,4 +88,6 @@ parameters_filter_cached_coordinates_not_empty_tiles_dir = \
      ([(-1024, 1024), (-768, 1024)],
       []),
      ([(-256, 256), (0, 256)],
-      [])]
+      [])
+     # endregion
+     ]
