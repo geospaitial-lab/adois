755cee5c202d12da39c4121fb32988d159c8cc7f
mrsmrynk <mrsmrynk@gmail.com>
2022-11-24 23:36:02 +0100

    refactor

--- src/postprocessing/tests/data/data_test_fill_polygon.py
+++ src/postprocessing/tests/data/data_test_fill_polygon.py
@@ -3,72 +3,52 @@
 from shapely.geometry import Polygon
 
 
-polygon_coordinates_0 = [[-.25, -.25],
-                         [.25, -.25],
-                         [.25, .25],
-                         [-.25, .25]]
-polygon_coordinates_1 = [[-2.5, -2.5],
-                         [-1.5, -2.5],
-                         [-1.5, -1.5],
-                         [-2.5, -1.5]]
-polygon_coordinates_2 = [[-5.5, -5.5],
-                         [-3.5, -5.5],
-                         [-3.5, -3.5],
-                         [-5.5, -3.5]]
-polygon_coordinates_3 = [[1.5, -2.5],
-                         [2.5, -2.5],
-                         [2.5, -1.5],
-                         [1.5, -1.5]]
-polygon_coordinates_4 = [[3.5, -5.5],
-                         [5.5, -5.5],
-                         [5.5, -3.5],
-                         [3.5, -3.5]]
-polygon_coordinates_5 = [[1.5, 1.5],
-                         [2.5, 1.5],
-                         [2.5, 2.5],
-                         [1.5, 2.5]]
-polygon_coordinates_6 = [[3.5, 3.5],
-                         [5.5, 3.5],
-                         [5.5, 5.5],
-                         [3.5, 5.5]]
-polygon_coordinates_7 = [[-2.5, 1.5],
-                         [-1.5, 1.5],
-                         [-1.5, 2.5],
-                         [-2.5, 2.5]]
-polygon_coordinates_8 = [[-5.5, 3.5],
-                         [-3.5, 3.5],
-                         [-3.5, 5.5],
-                         [-5.5, 5.5]]
+polygon_interior_coordinates = [[[-.25, -.25],
+                                 [.25, -.25],
+                                 [.25, .25],
+                                 [-.25, .25]],
+                                [[-2.5, -2.5],
+                                 [-1.5, -2.5],
+                                 [-1.5, -1.5],
+                                 [-2.5, -1.5]],
+                                [[-5.5, -5.5],
+                                 [-3.5, -5.5],
+                                 [-3.5, -3.5],
+                                 [-5.5, -3.5]],
+                                [[1.5, -2.5],
+                                 [2.5, -2.5],
+                                 [2.5, -1.5],
+                                 [1.5, -1.5]],
+                                [[3.5, -5.5],
+                                 [5.5, -5.5],
+                                 [5.5, -3.5],
+                                 [3.5, -3.5]],
+                                [[1.5, 1.5],
+                                 [2.5, 1.5],
+                                 [2.5, 2.5],
+                                 [1.5, 2.5]],
+                                [[3.5, 3.5],
+                                 [5.5, 3.5],
+                                 [5.5, 5.5],
+                                 [3.5, 5.5]],
+                                [[-2.5, 1.5],
+                                 [-1.5, 1.5],
+                                 [-1.5, 2.5],
+                                 [-2.5, 2.5]],
+                                [[-5.5, 3.5],
+                                 [-3.5, 3.5],
+                                 [-3.5, 5.5],
+                                 [-5.5, 5.5]]]
 
-polygon_coordinates = [[-6.5, -6.5],
-                       [6.5, -6.5],
-                       [6.5, 6.5],
-                       [-6.5, 6.5]]
+polygon_exterior_coordinates = [[-6.5, -6.5],
+                                [6.5, -6.5],
+                                [6.5, 6.5],
+                                [-6.5, 6.5]]
 
-fill_polygon_polygon = Polygon(polygon_coordinates,
-                               holes=[polygon_coordinates_0,
-                                      polygon_coordinates_1,
-                                      polygon_coordinates_2,
-                                      polygon_coordinates_3,
-                                      polygon_coordinates_4,
-                                      polygon_coordinates_5,
-                                      polygon_coordinates_6,
-                                      polygon_coordinates_7,
-                                      polygon_coordinates_8])
-filled_polygon_1 = Polygon(polygon_coordinates,
-                           holes=[polygon_coordinates_1,
-                                  polygon_coordinates_2,
-                                  polygon_coordinates_3,
-                                  polygon_coordinates_4,
-                                  polygon_coordinates_5,
-                                  polygon_coordinates_6,
-                                  polygon_coordinates_7,
-                                  polygon_coordinates_8])
-filled_polygon_10 = Polygon(polygon_coordinates)
+polygon = Polygon(polygon_exterior_coordinates, holes=polygon_interior_coordinates)
+filled_polygon_1 = Polygon(polygon_exterior_coordinates, holes=polygon_interior_coordinates[1:])
+filled_polygon_10 = Polygon(polygon_exterior_coordinates)
 
-parameters_fill_polygon = \
-    [
-        ((fill_polygon_polygon, 0), fill_polygon_polygon),
-        ((fill_polygon_polygon, 1), filled_polygon_1),
-        ((fill_polygon_polygon, 10), filled_polygon_10)
-    ]
+parameters_fill_polygon = [((polygon, 0), polygon),
+                           ((polygon, 1), filled_polygon_1),
+                           ((polygon, 10), filled_polygon_10)]
