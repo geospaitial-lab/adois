7da0ee64fd02684ed71b5c3bcbd3e5893a7faa2f
mrsmrynk <mrsmrynk@gmail.com>
2023-03-12 18:42:52 +0100

    change class names

--- src/postprocessing/tests/data/data_test_fill_gdf.py
+++ src/postprocessing/tests/data/data_test_fill_gdf.py
@@ -40,15 +40,15 @@ polygon = Polygon(polygon_exterior_coordinates,
                   holes=[list(polygon_interior.exterior.coords) for polygon_interior in polygons_interior])
 
 gdf_buildings = gpd.GeoDataFrame(geometry=[polygon], crs='EPSG:25832')
-gdf_buildings['class'] = [1]
+gdf_buildings['class'] = ['Hochbau']
 
-gdf_surfaces = gpd.GeoDataFrame(geometry=[polygon], crs='EPSG:25832')
-gdf_surfaces['class'] = [2]
+gdf_pavements = gpd.GeoDataFrame(geometry=[polygon], crs='EPSG:25832')
+gdf_pavements['class'] = ['Tiefbau']
 
 gdf = gpd.GeoDataFrame(geometry=[shapely.affinity.affine_transform(polygon, matrix=[1., 0., 0., 1., -14.25, 0.]),
                                  shapely.affinity.affine_transform(polygon, matrix=[1., 0., 0., 1., 14.25, 0.])],
                        crs='EPSG:25832')
-gdf['class'] = [1, 2]
+gdf['class'] = ['Hochbau', 'Tiefbau']
 
 filled_polygon_buildings_1 = Polygon(polygon_exterior_coordinates,
                                      holes=[list(polygon_interior.exterior.coords) for polygon_interior in
@@ -58,43 +58,43 @@ filled_polygon_buildings_10 = Polygon(polygon_exterior_coordinates,
                                              polygons_interior if polygon_interior.area >= 20])
 
 filled_gdf_buildings_1 = gpd.GeoDataFrame(geometry=[filled_polygon_buildings_1], crs='EPSG:25832')
-filled_gdf_buildings_1['class'] = [1]
+filled_gdf_buildings_1['class'] = ['Hochbau']
 filled_gdf_buildings_10 = gpd.GeoDataFrame(geometry=[filled_polygon_buildings_10], crs='EPSG:25832')
-filled_gdf_buildings_10['class'] = [1]
+filled_gdf_buildings_10['class'] = ['Hochbau']
 
-filled_polygon_surfaces_1 = Polygon(polygon_exterior_coordinates,
+filled_polygon_pavements_1 = Polygon(polygon_exterior_coordinates,
                                     holes=[list(polygon_interior.exterior.coords) for polygon_interior in
                                            polygons_interior if polygon_interior.area >= 1])
-filled_polygon_surfaces_10 = Polygon(polygon_exterior_coordinates,
+filled_polygon_pavements_10 = Polygon(polygon_exterior_coordinates,
                                      holes=[list(polygon_interior.exterior.coords) for polygon_interior in
                                             polygons_interior if polygon_interior.area >= 10])
 
-filled_gdf_surfaces_1 = gpd.GeoDataFrame(geometry=[filled_polygon_surfaces_1], crs='EPSG:25832')
-filled_gdf_surfaces_1['class'] = [2]
-filled_gdf_surfaces_10 = gpd.GeoDataFrame(geometry=[filled_polygon_surfaces_10], crs='EPSG:25832')
-filled_gdf_surfaces_10['class'] = [2]
+filled_gdf_pavements_1 = gpd.GeoDataFrame(geometry=[filled_polygon_pavements_1], crs='EPSG:25832')
+filled_gdf_pavements_1['class'] = ['Tiefbau']
+filled_gdf_pavements_10 = gpd.GeoDataFrame(geometry=[filled_polygon_pavements_10], crs='EPSG:25832')
+filled_gdf_pavements_10['class'] = ['Tiefbau']
 
 filled_gdf_1 = gpd.GeoDataFrame(geometry=[shapely.affinity.affine_transform(filled_polygon_buildings_1,
                                                                             matrix=[1., 0., 0., 1., -14.25, 0.]),
-                                          shapely.affinity.affine_transform(filled_polygon_surfaces_1,
+                                          shapely.affinity.affine_transform(filled_polygon_pavements_1,
                                                                             matrix=[1., 0., 0., 1., 14.25, 0.])],
                                 crs='EPSG:25832')
-filled_gdf_1['class'] = [1, 2]
+filled_gdf_1['class'] = ['Hochbau', 'Tiefbau']
 filled_gdf_10 = gpd.GeoDataFrame(geometry=[shapely.affinity.affine_transform(filled_polygon_buildings_10,
                                                                              matrix=[1., 0., 0., 1., -14.25, 0.]),
-                                           shapely.affinity.affine_transform(filled_polygon_surfaces_10,
+                                           shapely.affinity.affine_transform(filled_polygon_pavements_10,
                                                                              matrix=[1., 0., 0., 1., 14.25, 0.])],
                                  crs='EPSG:25832')
-filled_gdf_10['class'] = [1, 2]
+filled_gdf_10['class'] = ['Hochbau', 'Tiefbau']
 
 empty_gdf = gpd.GeoDataFrame(geometry=[], crs='EPSG:25832')
 
 parameters_fill_gdf = [((gdf_buildings, 0), gdf_buildings),
                        ((gdf_buildings, 1), filled_gdf_buildings_1),
                        ((gdf_buildings, 10), filled_gdf_buildings_10),
-                       ((gdf_surfaces, 0), gdf_surfaces),
-                       ((gdf_surfaces, 1), filled_gdf_surfaces_1),
-                       ((gdf_surfaces, 10), filled_gdf_surfaces_10),
+                       ((gdf_pavements, 0), gdf_pavements),
+                       ((gdf_pavements, 1), filled_gdf_pavements_1),
+                       ((gdf_pavements, 10), filled_gdf_pavements_10),
                        ((gdf, 0), gdf),
                        ((gdf, 1), filled_gdf_1),
                        ((gdf, 10), filled_gdf_10),
