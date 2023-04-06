92165c41b562ceb4600e6fb4c11406148f9aeea7
mrsmrynk <mrsmrynk@gmail.com>
2023-04-06 14:18:31 +0200

    add border clipping

--- src/inference/inference.py
+++ src/inference/inference.py
@@ -1,18 +1,35 @@
 import numpy as np
 import onnxruntime
 
+import src.utils.settings as settings
+
 
 class Inference:
-    def __init__(self, model_path):
+    def __init__(self,
+                 model_path,
+                 clip_border):
         """
         | Constructor method
 
         :param str model_path: path to the model (.onnx)
+        :param bool clip_border: if True, the mask is clipped
         :returns: None
         :rtype: None
         """
         self.model = onnxruntime.InferenceSession(model_path)
         self.model_input_name = self.model.get_inputs()[0].name
+        self.clip_border = clip_border
+
+    @staticmethod
+    def clip_mask(mask):
+        """
+        | Returns a clipped mask.
+
+        :param np.ndarray[np.uint8] mask: mask
+        :returns: clipped mask
+        :rtype: np.ndarray[np.uint8]
+        """
+        return np.array(mask[settings.BORDER_SIZE:-settings.BORDER_SIZE, settings.BORDER_SIZE:-settings.BORDER_SIZE])
 
     def get_mask(self, image):
         """
@@ -29,4 +46,7 @@ class Inference:
         mask = np.array(self.model.run([], {self.model_input_name: model_input}))
         mask = np.squeeze(mask)
         mask = np.argmax(mask, axis=-1).astype(np.uint8)
+
+        if self.clip_border:
+            mask = self.clip_mask(mask)
         return mask
