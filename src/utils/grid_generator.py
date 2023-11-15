2dc1683312fff3e9dbfc64e45dac5cd1a0276262
mrsmrynk <mrsmrynk@gmail.com>
2023-11-15 15:05:03 +0100

    refactor

--- src/utils/grid_generator.py
+++ src/utils/grid_generator.py
@@ -79,7 +79,7 @@ class GridGenerator:
         polygons = [Box(x_min, y_min, x_min + tile_size, y_min + tile_size)
                     for x_min, y_min in coordinates]
 
-        gdf_grid = gpd.GeoDataFrame(geometry=polygons,
-                                    crs=f'EPSG:{self.epsg_code}')
+        grid = gpd.GeoDataFrame(geometry=polygons,
+                                crs=f'EPSG:{self.epsg_code}')
 
-        return gdf_grid
+        return grid
