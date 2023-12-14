a80f09d28e73610a264e2879922e6d0132f2e02a
mrsmrynk <mrsmrynk@gmail.com>
2023-12-14 21:29:32 +0100

    add type hinting

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -1,5 +1,5 @@
-PADDING_SIZE = 320  # padding size in pixels (type: int)
-PADDING_SIZE_METERS = 64  # padding size in meters (type: int)
-IMAGE_SIZE = 1280  # image size in pixels (type: int)
-IMAGE_SIZE_METERS = 256  # image size in meters (type: int)
-RESOLUTION = .2  # resolution in meters per pixel (type: float)
+PADDING_SIZE: int = 320  # padding size in pixels
+PADDING_SIZE_METERS: int = 64  # padding size in meters
+IMAGE_SIZE: int = 1280  # image size in pixels
+IMAGE_SIZE_METERS: int = 256  # image size in meters
+RESOLUTION: float = .2  # resolution in meters per pixel
