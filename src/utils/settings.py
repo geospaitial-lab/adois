4f353836dd2ff1febab0d419cd29270c9cffb37a
mrsmrynk <mrsmrynk@gmail.com>
2023-12-15 16:26:53 +0100

    refactor

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -1,5 +1,5 @@
-PADDING_SIZE: int = 320  # padding size in pixels
-PADDING_SIZE_METERS: int = 64  # padding size in meters
 IMAGE_SIZE: int = 1280  # image size in pixels
 IMAGE_SIZE_METERS: int = 256  # image size in meters
+PADDING_SIZE: int = 320  # padding size in pixels
+PADDING_SIZE_METERS: int = 64  # padding size in meters
 RESOLUTION: float = .2  # resolution in meters per pixel
