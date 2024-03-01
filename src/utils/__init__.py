03895968ab1e5f9eb35d57869236a95d15c8cf16
mrsmrynk <marius.maryniak@w-hs.de>
2024-03-01 14:05:29 +0100

    add exporter

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,2 +1,3 @@
 from .coordinator import Coordinator
+from .exporter import Exporter
 from .grid_generator import GridGenerator
---
+++ src/utils/exporter.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+from pathlib import Path
+
+import geopandas as gpd
+
+
+class Exporter:
+
+    def __init__(self,
+                 path_output_dir: Path) -> None:
+        """
+        | Initializer method
+
+        :param path_output_dir: path to the output directory
+        :returns: None
+        """
+        assert isinstance(path_output_dir, Path)
+
+        self.path_output_dir = path_output_dir / str(datetime.now().isoformat(sep='_', timespec='seconds'))
+
+    def create_output_dir(self) -> None:
+        """
+        | Creates the output directory.
+
+        :returns: None
+        """
+        self.path_output_dir.mkdir()
+
+    def export_gdf(self,
+                   gdf: gpd.GeoDataFrame,
+                   relative_path: Path) -> None:
+        """
+        | Exports the geodataframe as a geopackage.
+
+        :param gdf: geodataframe
+        :param relative_path: relative path
+        :returns: None
+        """
+        assert isinstance(gdf, gpd.GeoDataFrame)
+
+        assert isinstance(relative_path, Path)
+
+        path = self.path_output_dir / relative_path
+
+        if len(relative_path.parts) > 1:
+            path.parents[0].mkdir(parents=True,
+                                  exist_ok=True)
+
+        gdf.to_file(str(path), driver='GPKG')
